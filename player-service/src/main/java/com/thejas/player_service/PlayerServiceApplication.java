0a688eb482046876148f5c88134bfd0424200a6f
thejas01 <thejasgs19@gmail.com>
2025-04-21 10:11:36 +0530

    changes made for inter service

--- player-service/src/main/java/com/thejas/player_service/PlayerServiceApplication.java
+++ player-service/src/main/java/com/thejas/player_service/PlayerServiceApplication.java
@@ -2,8 +2,10 @@ package com.thejas.player_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlayerServiceApplication {
 
 	public static void main(String[] args) {
